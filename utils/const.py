dc0d2f83eab15ea867086db445a44ed7e35bf907
fluidcat <Belin0@163.com>
2025-03-30 15:02:20 +0800

    feat：PLUGIN_HANDLED问题

--- utils/const.py
+++ utils/const.py
@@ -1,4 +1,4 @@
 NUMBERS = ['⓪', '①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩', '⑪', '⑫', '⑬', '⑭', '⑮', '⑯', '⑰', '⑱', '⑲', '⑳']
 PLUGIN_PASS = True
-PLUGIN_HANDLED = True
+PLUGIN_HANDLED = 'handled'
 PLUGIN_ENDED = False
